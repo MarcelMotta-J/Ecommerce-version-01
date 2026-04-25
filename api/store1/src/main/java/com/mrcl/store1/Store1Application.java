2055255a83b5c7fe200ec78016006c052b2d01ec
MarcelMotta-J <marceldsmotta@gmail.com>
2026-04-24 21:22:16 -0300

    fix mobile cart header layout

--- api/store1/src/main/java/com/mrcl/store1/Store1Application.java
+++ api/store1/src/main/java/com/mrcl/store1/Store1Application.java
@@ -1,12 +1,16 @@
 package com.mrcl.store1;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
+
 @EnableCaching
 @SpringBootApplication
 public class Store1Application {
+
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(Store1Application.class, args);
@@ -14,6 +18,13 @@ public class Store1Application {
 		System.out.println("MySql:");
 		System.out.println("DB MySql username: " + System.getenv("DB_USERNAME_MYSQL"));
 		System.out.println("DB MySql password: " + System.getenv("DB_PASSWORD_MYSQL"));
+
+
+
+
 	}
+
+
+
 
 }
